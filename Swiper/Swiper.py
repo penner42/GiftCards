8f7e54ceb97bad949101d9284fb23de2c7a1f3fd
Alan Penner <metsfan@alumni.cmu.edu>
2017-11-29 08:43:09 -0800

    Combine PIN and card into one field. Type PIN, swipe card.

--- Swiper/Swiper.py
+++ Swiper/Swiper.py
@@ -7,33 +7,31 @@ from kivy.uix.button import Button
 
 class InputWindow(BoxLayout):
 
-    def focus_swipefield(self, dt):
-        self.ids.swipefield.focus = True
+    def focus_inputfield(self, dt):
+        self.ids.inputfield.focus = True
 
-    def focus_pinfield(self, dt):
-        self.ids.pinfield.focus = True
+    def inputfield_entered(self):
+        # card not swiped
+        if "%B" not in self.ids.inputfield.text:
+            Clock.schedule_once(self.focus_inputfield, -1)
+        else:
+            pin, data = self.ids.inputfield.text.split("%B")
+            self.card_swiped(pin, data)
 
-    def pin_entered(self):
-        Clock.schedule_once(self.focus_swipefield, -1)
-
-    def card_swiped(self):
-        if self.ids.swipefield.text.count('?') != 2:
-            self.ids.swipefield.text = ""
-            self.ids.swipefield.hint_text = "Swipe again"
-            Clock.schedule_once(self.focus_swipefield, -1)
-            return
-
-        card_no = self.detect(self.ids.swipefield.text)
-        self.ids.csv_output.text += card_no + "," + self.ids.pinfield.text.strip() + "\r\n"
-        self.ids.pinfield.text = ""
-        self.ids.swipefield.text = ""
-        self.ids.swipefield.hint_text = "Swipe card here"
-        Clock.schedule_once(self.focus_pinfield, -1)
+    def card_swiped(self, pin, data):
+        if data.count('?') != 2:
+            self.ids.inputfield.text = pin
+            Clock.schedule_once(self.focus_inputfield, -1)
+        else:
+            card_no = self.detect(data)
+            self.ids.csv_output.text += card_no + "," + pin + "\r\n"
+            self.ids.inputfield.text = ""
+            Clock.schedule_once(self.focus_inputfield, -1)
 
     def clear_release(self, value):
         if value == "normal":
             self.ids.csv_output.text = ""
-            Clock.schedule_once(self.focus_pinfield, -1)
+            Clock.schedule_once(self.focus_inputfield, -1)
 
     def copy_output(self, value):
         if value == "normal":
@@ -44,9 +42,9 @@ class InputWindow(BoxLayout):
         [t1,t2,t3] = [x.strip() for x in data.split('?')]
 
         if (t1[-4:] == t2[-4:]) and (t1[-4:] != "0000"):
-            cardno = sec1[2]+sec3[14:16]+sec1[8:17]+sec3[16:20]
+            cardno = sec1[0]+sec3[12:14]+sec1[6:15]+sec3[14:18]
         else:
-            cardno = sec1[2:22]
+            cardno = sec1[0:20]
 
         return ' '.join(cardno[i:i+4] for i in range(0,len(cardno), 4))
 
