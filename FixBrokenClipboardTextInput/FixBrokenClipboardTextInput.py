f0067c60a84684f20a1657f0117b2426cf7ab508
Alan Penner <metsfan@alumni.cmu.edu>
2018-02-15 16:16:27 -0800

    Workaround for broken clipboard.

---
+++ FixBrokenClipboardTextInput/FixBrokenClipboardTextInput.py
@@ -0,0 +1,13 @@
+from kivy.uix.textinput import TextInput
+import tkinter as tk
+
+
+class FixBrokenClipboardTextInput(TextInput):
+    def __init__(self, **kwargs):
+        super(FixBrokenClipboardTextInput, self).__init__(**kwargs)
+
+    def copy(self, data=''):
+        # use tkinter here because Kivy clipboard is broken
+        tkwin = tk.Tk()
+        tkwin.withdraw()
+        tkwin.clipboard_append(self.selection_text)
