6d0de4d963cf68024de443dc7dede29f5d6cdf55
apenner <apenner@gmail.com>
2018-11-21 07:49:01 -0800

    tkinter progress

--- test.py
+++ test.py
@@ -4,39 +4,6 @@ from tkinter.scrolledtext import ScrolledText
 from Extract.Extract import ExtractFrame
 import configparser
 
-#
-# class Extract(tk.Frame):
-#     def __init__(self, parent, *args, **kwargs):
-#         super().__init__(parent, *args, **kwargs)
-#
-#         # Create a Tkinter variable
-#         tkvar = StringVar(parent)
-#
-#         # Dictionary with options
-#         self.checkboxes = [IntVar() for e in extractors.extractors_list]
-#
-#         choices = [e.name() for e in extractors.extractors_list]
-#         for i, e in enumerate(extractors.extractors_list):
-#             Checkbutton(self, text=e.name(), variable=self.checkboxes[i]).grid(row=i, sticky=W)
-#
-#         tkvar.set(extractors.extractors_list[0].name())  # set the default option
-#
-#         Button(self, text='Show', command=self.checked).grid(row=len(choices), sticky=W, pady=4)
-#
-#         # popupMenu = OptionMenu(page1, tkvar, *choices)
-#         # popupMenu.grid(row=0, column=0)
-#
-#         # on change dropdown value
-#         def change_dropdown(*args):
-#             print(tkvar.get())
-#
-#         # link function to change dropdown
-#         tkvar.trace('w', change_dropdown)
-#
-#     def checked(self):
-#         print([c.get() for c in self.checkboxes])
-
-
 class GiftCards(tk.Tk):
     def __init__(self):
         super().__init__()
