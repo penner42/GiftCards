cfe84b2060090291f4741f24335572032a2dd165
Alan Penner <metsfan@alumni.cmu.edu>
2018-11-30 10:09:25 -0800

    Chromedriver file selector.

--- EntryWithHintText.py
+++ EntryWithHintText.py
@@ -5,6 +5,9 @@ import threading
 
 class EntryWithHintText(Entry):
     def __init__(self, master=None, hint='Hint Text', hint_color='grey', **args):
+        self.trace_id = None
+        self.insert_called = False
+
         s = Style()
         self.style_name = str(id(self)) + 'EntryWithHintText.TEntry'
         s.configure(self.style_name, foreground=hint_color, selectborderwidth=0,
@@ -22,14 +25,25 @@ class EntryWithHintText(Entry):
         self.bind("<B1-Motion>", self.check_cursor)
         self.bind("<Left>", self.check_cursor)
         self.bind("<Right>", self.check_cursor)
+        self.add_trace()
+
+    def add_trace(self):
         self.trace_id = self.text.trace('w', self.entry_changed)
-        self.process_change = True
+
+    def remove_trace(self):
+        if self.trace_id:
+            self.text.trace_vdelete('w', self.trace_id)
+            self.trace_id = None
 
     def insert(self, index, string):
-        self.process_change = False
-        self.delete(0, END)
-        super().insert(index, string)
-        self.process_change = True
+        self.insert_called = True
+
+        if not self.modified:
+            self.text.set(string)
+            self.insert_called = False
+        else:
+            super().insert(index, string)
+            self.insert_called = False
 
     def check_cursor(self, *args):
         if not self.modified:
@@ -39,7 +53,7 @@ class EntryWithHintText(Entry):
     def put_hint(self):
         self.modified = False
         self.configure(style=self.style_name)
-        self.delete(0, END)
+        # self.delete(0, END)
         self.insert(0, self.hint)
         self.selection_range(0, 0)
         threading.Thread(target=lambda: self.icursor(0)).start()
@@ -50,22 +64,22 @@ class EntryWithHintText(Entry):
             threading.Thread(target=lambda: self.icursor(0)).start()
 
     def entry_changed(self, *args):
-        if not self.process_change:
-            return
-
+        self.remove_trace()
         if not self.modified:
             # if the user tried to delete something from the hint, fail
             if len(self.text.get()) < len(self.hint):
                 self.put_hint()
             else:
-                text = self.text.get()[self.index(INSERT)-1]
+                if not self.insert_called:
+                    self.text.set(self.text.get()[self.index(INSERT)-1])
+                    self.insert_called = False
                 self.configure(style='TEntry')
-                self.delete(0, END)
-                self.insert(INSERT, text)
                 self.modified = True
         else:
             if not self.text.get():
                 self.put_hint()
+
+        self.add_trace()
 
     def foc_in(self, *args):
         if not self.modified:
--- Settings.py
+++ Settings.py
@@ -1,16 +1,21 @@
-import re
 from tkinter import *
-from tkinter.scrolledtext import ScrolledText
 from tkinter.ttk import *
 from EntryWithHintText import EntryWithHintText
+from tkinter.filedialog import askopenfilename
 
 class SettingsFrame(Frame):
     def __init__(self, parent):
         super().__init__(parent)
 
         self._settings = self.winfo_toplevel().get_settings()
+        self.columnconfigure(1, weight=1)
+        Label(self, text='ChromeDriver Executable').grid(row=0, column=0, pady=5, columnspan=5, sticky=W)
+        self.chromedriver_filename = EntryWithHintText(self,hint='Path to ChromeDriver executable.')
+        self.chromedriver_filename.grid(row=1, column=1, sticky=E+W)
+        Button(self, text='Choose File', command=self.choose_file).grid(row=1, column=0)
 
-        Label(self, text='Chromedriver Executable').grid(row=0, column=0)
-        test = EntryWithHintText(self, hint='asdf')
-        test.insert(0, 'blah')
-        test.grid()
+    def choose_file(self):
+        filename = askopenfilename(title='Select chromedriver.exe', filetypes=[("", "*.exe")])
+        if filename != '':
+            self.chromedriver_filename.delete(0, END)
+            self.chromedriver_filename.insert(INSERT, filename)
